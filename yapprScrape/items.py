49db528b7cc1f225f2feb1524a6b0e51fb4f6c49
Žiga Sedovnik <ziga.sedovnik@outfit7.com>
2019-06-04 12:58:37 +0200

    first commit

---
+++ yapprScrape/items.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class YapprItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    group = scrapy.Field()
+    english = scrapy.Field()
+    french = scrapy.Field()
+    german = scrapy.Field()
+    spanish = scrapy.Field()
+    section = scrapy.Field()
+    subsection = scrapy.Field()
+    date_of_acceptance = scrapy.Field()
+    official_lang = scrapy.Field()
+    date_of_publish = scrapy.Field()
+    status = scrapy.Field()
+    country = scrapy.Field()
+    working_trial = scrapy.Field()
---
+++ yapprScrape/spiders/yappr_spider.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from yapprScrape.items import YapprItem
+
+
+class YapprSpiderSpider(scrapy.Spider):
+    name = 'yappr_spider'
+    main_url = "http://www.fci.be"
+
+    start_urls = [main_url + '/en/Nomenclature/Default.aspx/']
+
+    def parse(self, response):
+        # get letters
+        letters = response.xpath("//ul[@class='initiales']/li/a/text()").extract()
+
+        # scrape letters
+        # for l in [letters[0]]:
+        for l in letters:
+            yield scrapy.Request(self.main_url + "/en/nomenclature/races.aspx?init=" + l, callback=self.parse_letter)
+
+    def parse_letter(self,response):
+        # get breed links
+        breed_links = response.xpath('//ul[@class="listeraces"]/li/a/@href').extract()
+
+        # for b in breed_links[0:2]:
+        for b in breed_links:
+            yield scrapy.Request(self.main_url + b, callback=self.parse_breed)
+
+    def parse_breed(self, response):
+        group = response.xpath('//*[@id="ContentPlaceHolder1_GroupeHyperLink"]//text()').get()
+
+        english = response.xpath('//*[@id="ContentPlaceHolder1_NomEnLabel"]/text()').get()
+        french = response.xpath('//*[@id="ContentPlaceHolder1_NomFRLabel"]/text()').get()
+        german = response.xpath('//*[@id="ContentPlaceHolder1_NomDELabel"]/text()').get()
+        spanish = response.xpath('//*[@id="ContentPlaceHolder1_NomESLabel"]/text()').get()
+
+        section = response.xpath('//*[@id="ContentPlaceHolder1_SectionLabel"]/text()').get()
+        subsection = response.xpath('//*[@id="ContentPlaceHolder1_SousSectionLabel"]/text()').get()
+        date_of_acceptance = response.xpath('//*[@id="ContentPlaceHolder1_DateReconnaissanceLabel"]/text()').get()
+        official_lang = response.xpath('//*[@id="ContentPlaceHolder1_LangueOrigineLabel"]/text()').get()
+        date_of_publish = response.xpath('//*[@id="ContentPlaceHolder1_DateStandardEnVigueurLabel"]/text()').get()
+        status = response.xpath('//*[@id="ContentPlaceHolder1_StatutLabel"]/text()').get()
+        country = response.xpath('//*[@id="ContentPlaceHolder1_PaysOrigineLabel"]/text()').get()
+        working_trial = response.xpath('//*[@id="ContentPlaceHolder1_EpreuveTravailLabel"]/text()').get()
+
+        item = YapprItem()
+        item["group"] = group
+        item["english"] = english
+        item["french"] = french
+        item["german"] = german
+        item["spanish"] = spanish
+        item["section"] = section
+        item["subsection"] = subsection
+        item["date_of_acceptance"] = date_of_acceptance
+        item["official_lang"] = official_lang
+        item["date_of_publish"] = date_of_publish
+        item["status"] = status
+        item["country"] = country
+        item["working_trial"] = working_trial
+        yield item
